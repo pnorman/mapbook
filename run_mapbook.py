c4c120f3843980ef55ad7e5ef190ad3d5c7b80bc
Paul Norman <penorman@mac.com>
2012-02-21 13:52:47 -0800

    Now calling mapnik and zooming to a box

--- run_mapbook.py
+++ run_mapbook.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 
 class Page:
@@ -23,12 +23,13 @@ if __name__ == "__main__":
  
 	parser = LineArgumentParser(description='Create a mapbook',fromfile_prefix_chars='@')
 	
-	parser.add_argument('--startx', type=float, help='West coordinate to map in mercator km')
-	parser.add_argument('--starty', type=float, help='South coordinate to map in mercator km')
-	parser.add_argument('--width', type=float, help='Width in mercator km of a map page')
-	parser.add_argument('--overwidth', type=float, help='Width in mercator km to add to each side')
+	parser.add_argument('--startx', type=float, help='West coordinate to map in mercator km',required=True)
+	parser.add_argument('--starty', type=float, help='South coordinate to map in mercator km',required=True)
+	parser.add_argument('--width', type=float, help='Width in mercator km of a map page',required=True)
+	parser.add_argument('--overwidth', type=float, help='Width in mercator km to add to each side', default=0.)
 	parser.add_argument('--pagewidth', type=float, help='Page width in points. Should be <= physical page width',required=True)
 	parser.add_argument('--pageheight', type=float, help='Page height in points. Should be <= physical page height',required=True)
+	parser.add_argument('--mapfile',help='Mapnik XML file')
 	opts=parser.parse_args()
 
 	print opts
@@ -36,9 +37,14 @@ if __name__ == "__main__":
 	import mapnik2 as mapnik
 	import cairo
 	
-	padding=10
+	padding=10.
 	mapwidth=opts.pagewidth-2*padding
 	mapheight=opts.pageheight-2*padding
+	# minx, miny, maxx, maxy
+	bounds = (opts.startx, opts.starty, opts.startx+opts.width, opts.starty+opts.width*(mapheight/mapwidth))
+	m = mapnik.Map(int(mapwidth),int(mapheight),'+init=epsg:900913')
+	mapnik.load_map(m,opts.mapfile)
+	m.zoom_to_box(mapnik.Box2d(*bounds))
 	
 	surface = cairo.PDFSurface("test.pdf",opts.pagewidth,opts.pageheight)
 	book = Page(cairo.PDFSurface("test.pdf",opts.pagewidth,opts.pageheight),0)
@@ -50,6 +56,6 @@ if __name__ == "__main__":
 	cr.rectangle(10,10,mapwidth,mapheight)
 	cr.stroke()
 	
-	 
+	
 
 	
