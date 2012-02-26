2dcfc530f0c0ab8e0aea2bcf439e0f11ee42b6ad
Paul Norman <penorman@mac.com>
2012-02-26 15:24:24 -0800

    Release mapbook under GPL v3 or later

--- mapbook.py
+++ mapbook.py
@@ -1,5 +1,25 @@
 #!/usr/bin/env python
 
+'''
+	This file is part of mapbook.
+
+	mapbook is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	mapbook is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+	Copyright 2012 Paul Norman
+'''
+
+
 class Page:
 	def __init__(self, mapnumber, minx, miny, width, ratio):
 	
